fe57ac3f996fe9e203ad4cb4bf30dc20829e28c2
Kinnan Kwok <kinnankwok@Kinnans-MacBook-Air.local>
2014-09-24 16:26:05 -0700

    AsdfSA

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,7 @@
+def is_picture?(filename)
+	filename =~ /([^\s]+(\.(?i)(JPG|jpg|PNG|png|GIF|gif|BMP|bmp))$)/
+end
+
 # Homepage (Root path)
 get '/' do
   erb :index
@@ -6,7 +10,7 @@ end
 	
 # Display all photos
 get '/photos' do
-  erb:'/photo/photo'
+  erb :'/photo/photo'
 end
 	
 	
@@ -19,25 +23,32 @@ end
 # Create a new photos
 post '/photos' do
 
-	# save the File object into /images
-	File.open('public/uploads/' + params[:photo][:filename], "w") do |f|
-    f.write(params[:photo][:tempfile].read)
-  end
+	# Put the image into /uploads only if it is a picture
+	if params[:photo] && is_picture?(params[:photo][:filename])
+		# save the File object into /images
+		File.open('public/uploads/' + params[:photo][:filename], "w") do |f|
+	    f.write(params[:photo][:tempfile].read)
+	  end
 
-  redirect '/photos'
+	  redirect '/photos'
+	else
+		# set an error flag to display "Was not an image file" to the form
+		@error = true
+		erb :'/photo/new'
+	end
 
 end
 	
 
 # Show photo with id = #
 get '/photos/:id' do
-  erb:'/photo/show'
+  erb :'/photo/show'
 end
 
 
 #Filtered photos page with a single tags
 get '/photos/tag/:id' do
-  erb:'/photo/tag'
+  erb :'/photo/tag'
 end
 
 
