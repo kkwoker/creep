e34d6cf00ea8f5972b6f2175eaa6dea305384b65
Kinnan Kwok <kinnankwok@Kinnans-MacBook-Air.local>
2014-09-23 22:56:33 -0700

    Added some RESTful urls

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,47 @@
 get '/' do
   erb :index
 end
+
+
+
+
+# Display all pictures
+get '/pictures'
+
+end
+
+
+# Show form to create new picture
+get '/pictures/new'
+
+end
+
+
+# Create a new picture
+post '/pictures'
+
+end
+
+
+# Show picture with id = #
+get '/pictures/:id'
+
+end
+
+
+#Filtered pictures page with a single tags
+get '/pictures/tag/:id' do
+
+end
+
+
+#About
+get '/about' do
+
+end
+
+
+#Secret admin login
+get '/login' do
+
+end
