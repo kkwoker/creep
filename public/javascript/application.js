8618353b351231dd984118ffef5cc04017346d66
Kinnan Kwok <kinnankwok@Kinnans-MacBook-Air.local>
2014-09-25 19:44:39 -0700

    gmaps

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -37,3 +37,33 @@ $(document).ready(function() {
     }
    
 });
+
+
+var geocoder = new google.maps.Geocoder();
+
+function geocodePosition(pos) {
+  geocoder.geocode({
+    latLng: pos
+  }, function(responses) {
+    if (responses && responses.length > 0) {
+      updateMarkerAddress(responses[0].formatted_address);
+    } else {
+      updateMarkerAddress('Cannot determine address at this location.');
+    }
+  });
+}
+
+function updateMarkerStatus(str) {
+  document.getElementById('markerStatus').innerHTML = str;
+}
+
+function updateMarkerPosition(latLng) {
+  document.getElementById('info').innerHTML = [
+    latLng.lat(),
+    latLng.lng()
+  ].join(', ');
+}
+
+function updateMarkerAddress(str) {
+  document.getElementById('address').innerHTML = str;
+}
