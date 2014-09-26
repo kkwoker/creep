cbc326fc5bc8f18b54ac4e2f42b29c12c9bf75db
Kieran Cormack <kcormack91@gmail.com>
2014-09-26 23:01:22 +0000

    photocommments

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,12 @@ class Photo < ActiveRecord::Base
 
   has_many :comments
   has_and_belongs_to_many :tags
+
+
+  def most_recent_comment
+    if !self.comments.empty?
+      self.comments.order(:created_at).last
+    end
+  end
+
 end
